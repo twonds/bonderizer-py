00ab7423fccec495ea33f484273ae41e6b90cece
Christopher Zorn <christopher.zorn@gmail.com>
2023-02-27 18:26:50 -0800

    fire

---
+++ src/bonderizer/cli.py
@@ -0,0 +1,39 @@
+import fire
+
+
+class Bonderizer(object):
+    """
+    Bonds run time code or framework with a simple
+    interface and consumer and producer. This needs work.
+    """
+
+    def new(self):
+        """
+        Create a new project
+        """
+
+    def cli(self):
+        """
+        Generate CLI code for a project's runtime
+        """
+
+
+    def grpc(self):
+        """
+        Generate gRPC code for a project's runtime
+        """
+
+    def protobuf(self):
+        """
+        Generate a protobuf from a project's code
+        """
+
+    def openapi(self):
+        """
+        Generate openapi code and framework for a project's runtime
+        """
+
+
+
+if __name__ == '__main__':
+    fire.Fire()
